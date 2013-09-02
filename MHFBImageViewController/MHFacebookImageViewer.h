3b21b6f356a138cf068a3d9d6cff7fdf63c2af09
Joao Nunes <joao@Joaos-MacBook-Pro.local>
2013-09-02 11:58:14 +0300

    Changed open and close blocks from weak to strong

--- MHFBImageViewController/MHFacebookImageViewer.h
+++ MHFBImageViewController/MHFacebookImageViewer.h
@@ -41,8 +41,8 @@ typedef void (^MHFacebookImageViewerClosingBlock)(void);
 @property (weak, readonly, nonatomic) UIViewController *rootViewController;
 @property (nonatomic,strong) NSURL * imageURL;
 @property (nonatomic,strong) UIImageView * senderView;
-@property (nonatomic,weak) MHFacebookImageViewerOpeningBlock openingBlock;
-@property (nonatomic,weak) MHFacebookImageViewerClosingBlock closingBlock;
+@property (nonatomic,strong) MHFacebookImageViewerOpeningBlock openingBlock;
+@property (nonatomic,strong) MHFacebookImageViewerClosingBlock closingBlock;
 @property (nonatomic,weak) id<MHFacebookImageViewerDatasource> imageDatasource;
 @property (nonatomic,assign) NSInteger initialIndex;
 
