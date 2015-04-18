20c52736a7bcb2e432a8b6a5fd50f5f844664076
jsharp83 <jsharp83@gmail.com>
2015-04-18 12:23:33 +0900

    First Commit to develop rotation feature
    Add Example4

---
+++ FBImageViewController_Demo/FBImageViewController_Demo/Demo4ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Demo4ViewController.h
+//  FBImageViewController_Demo
+//
+//  Created by EunchulJeon on 2015. 4. 18..
+//  Copyright (c) 2015년 Michael Henry Pantaleon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Demo4ViewController : UITableViewController
+
+@end
