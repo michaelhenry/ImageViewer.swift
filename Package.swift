74162759f8e889b46d0f8660cfd548b673b20762
Ben Jessup <ben@superior.app>
2020-09-02 17:49:24 -0500

    Source path fix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/michaelhenry/ImageViewer.swift.git", from: "3.0.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
